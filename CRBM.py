ef7d961066b4216c8f335b838e6c66e0fea3b2d7
mfooladi <mfooladi@gmail.com>
2019-10-28 18:38:16 -0400

    finsihed

--- CRBM.py
+++ CRBM.py
@@ -7,14 +7,44 @@ np.set_printoptions(precision=5, edgeitems=25)
 
 
 class CRBM:
-    def __init__(self, weights):
-        self.weights = weights
+    def __init__(self, prob_name):
+        self.w1 = np.array([[[3.0, 2.9, 2.8, 2.7, 2.6],
+                   [2.9, 3.0, 2.9, 2.8, 2.7],
+                   [2.8, 2.9, 3.0, 2.9, 2.8],
+                   [2.7, 2.8, 2.9, 3.0, 2.9],
+                   [2.6, 2.7, 2.8, 2.9, 3.0]],
+                  [[3.0, 2.9, 2.8, 2.7, 2.6, 2.5, 2.4],
+                   [2.9, 3.0, 2.9, 2.8, 2.7, 2.6, 2.5],
+                   [2.8, 2.9, 3.0, 2.9, 2.8, 2.7, 2.6],
+                   [2.7, 2.8, 2.9, 3.0, 2.9, 2.8, 2.7],
+                   [2.6, 2.7, 2.8, 2.9, 3.0, 3.1, 3.2]]])
+
+        self.w2 = np.array([[[3.0, 2.9, 2.8, 2.7, 2.6],
+                             [2.6, 2.7, 2.8, 2.9, 3.0],
+                             [2.4, 2.5, 2.6, 2.7, 2.8],
+                             [2.4, 2.5, 2.6, 2.7, 2.8]],
+                            [[3.0, 2.9, 2.8, 2.7, 2.6],
+                             [2.4, 2.5, 2.6, 2.7, 2.8],
+                             [2.6, 2.7, 2.8, 2.9, 3.0],
+                             [2.4, 2.5, 2.6, 2.7, 2.8]],
+                            [[3.0, 2.9, 2.8, 2.7, 2.6],
+                             [2.4, 2.5, 2.6, 2.7, 2.8],
+                             [2.4, 2.5, 2.6, 2.7, 2.8],
+                             [2.6, 2.7, 2.8, 2.9, 3.0]]])
+
+        if prob_name == 'transition':
+            self.weights = self.w1
+        elif prob_name == 'emission':
+            self.weights = self.w2
+        else:
+            print('No probability name')
+
         self.hidden_units = len(self.weights[0])
         self.num_visible = len(self.weights)
         self.visible_states = list()
 
         for i in range(self.num_visible):
-            self.visible_states.append(int(np.size(weights[i]) / self.hidden_units))
+            self.visible_states.append(len(self.weights[i][0]))
 
         vs_copy = self.visible_states.copy()
         vs_copy.insert(0, self.hidden_units)
@@ -22,7 +52,7 @@ class CRBM:
         self.probs_shape = tuple(vs_copy)
         self.probs = np.zeros(self.probs_shape)
 
-        print()
+        print('CRBM initialized')
 
     def _net_input(self, states):
         e_net_sum = 0
@@ -42,61 +72,54 @@ class CRBM:
 
         prob_ = e_net / e_partition_func
 
-        print(prob_)
-
         return prob_
 
     def _a_ijk(self):
+        self.probs = np.zeros(self.probs_shape)
+
         for sj in range(5):
             for si in range(5):
                 for ul in range(7):
                     input_states = [sj, si, ul]
                     self.probs[sj, si, ul] = self._net_input(input_states)
 
-        return self.probs
+        return np.transpose(self.probs)
+
+    def total_transition_probs(self):
+        p1 = self._a_ijk()
+        p2 = self._a_ijk()
+        p3 = self._a_ijk()
+        total_trx_probs = np.kron(np.kron(p1, p2), p3)
+
+        return total_trx_probs
 
     def _o_jk(self):
-        # self.probs_o = np.zeros((4, 5, 5, 5))
+        self.probs = np.zeros(self.probs_shape)
+
         for yk in range(4):
             for s1 in range(5):
                 for s2 in range(5):
                     for s3 in range(5):
                         input_states = [yk, s1, s2, s3]
-                        print('input_states')
-                        print(input_states)
                         self.probs[yk, s1, s2, s3] = self._net_input(input_states)
-        return self.probs
+                        emission_reshape = self.probs.reshape(4, 125)
+
+        return emission_reshape
 
 
 if __name__ == '__main__':
-    w1 = np.array([[3.0, 2.9, 2.8, 2.7, 2.6],
-                   [2.9, 3.0, 2.9, 2.8, 2.7],
-                   [2.8, 2.9, 3.0, 2.9, 2.8],
-                   [2.7, 2.8, 2.9, 3.0, 2.9],
-                   [2.6, 2.7, 2.8, 2.9, 3.0]])
 
-    w2 = ([[3.0, 2.9, 2.8, 2.7, 2.6, 2.5, 2.4],
-           [2.9, 3.0, 2.9, 2.8, 2.7, 2.6, 2.5],
-           [2.8, 2.9, 3.0, 2.9, 2.8, 2.7, 2.6],
-           [2.7, 2.8, 2.9, 3.0, 2.9, 2.8, 2.7],
-           [2.6, 2.7, 2.8, 2.9, 3.0, 3.1, 3.2]])
-
-    ws = [w1, w2]
-
-    ws2 = np.random.random_sample((3, 4, 5))
-
-    r = CRBM(weights=ws2)
+    # r = CRBM('transition')
+    r = CRBM('emission')
 
     # probs = r._a_ijk()
-    probs = r._o_jk()
+    result = r._o_jk()
 
-    print(probs)
-
-    for i in range(4):
-        figure(i)
-        barchart(probs[i])
-
-    show()
+    # for i in range(4):
+    #     figure(i)
+    #     barchart(probs[i])
+    #
+    # show()
 
 
 
